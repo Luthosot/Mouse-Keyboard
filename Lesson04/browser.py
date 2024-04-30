18a2f5054741ac924853b21c64875bd2f9c49287
Lutho <127214522+Luthosot@users.noreply.github.com>
2024-04-30 16:02:50 +0200

    Delete Lesson04/browser.py
    useless

--- Lesson04/browser.py
+++
@@ -1,8 +0,0 @@
-'Browser automation with Selenium'
-from selenium import webdriver
-from selenium.webdriver.common.keys import keys
-import time
-
-driver = webdriver.chrome()
-driver.get('https://sinov8.net')
-
